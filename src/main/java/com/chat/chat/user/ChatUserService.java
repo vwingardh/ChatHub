4e2939df60b78e367cd0368e96c436695f900b3f
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-05 17:32:35 +0200

    Created methods for retrieving user and added exceptions

--- src/main/java/com/chat/chat/user/ChatUserService.java
+++ src/main/java/com/chat/chat/user/ChatUserService.java
@@ -1,10 +1,15 @@
 package com.chat.chat.user;
 
+import com.chat.chat.customExceptions.UsernameAlreadyTakenException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class ChatUserService {
 
     private final ChatUserRepository repo;
@@ -14,14 +19,39 @@ public class ChatUserService {
         this.repo = repo;
     }
 
-    public ChatUser createUser(String username) {
+    public ChatUser getUserByUsername(String username) throws NoSuchElementException {
+        ChatUser user = repo.findByUsername(username);
+        if (user == null) {
+            throw new NoSuchElementException(String.format("User with username '%s' does not exist", username));
+        }
+        return user;
+    }
+
+    public ChatUser createUser(String username) throws UsernameAlreadyTakenException {
+        ChatUser isUsernameAvailable = repo.findByUsername(username);
+        if (isUsernameAvailable != null) {
+            throw new UsernameAlreadyTakenException(username);
+        }
         ChatUser user = new ChatUser();
         user.setUsername(username);
         repo.save(user);
         return user;
     }
 
+    public ChatUser getUserById(Long id) throws NoSuchElementException {
+        Optional<ChatUser> chatUser = repo.findById(id);
+        if (!chatUser.isPresent()) {
+            throw new NoSuchElementException(String.format("User with id '%s' does not exist", id));
+        }
+        return chatUser.get();
+    }
+
+    public void deleteUserById(Long id) {
+        repo.deleteById(id);
+    }
+
     public List<ChatUser> getAllUsers() {
         return repo.findAll();
     }
+
 }
