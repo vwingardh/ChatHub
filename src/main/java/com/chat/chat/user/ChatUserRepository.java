355b8ef1abf98113991acbdb020271a9f229d2d3
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-05 17:31:01 +0200

    Created findByUsername() method

--- src/main/java/com/chat/chat/user/ChatUserRepository.java
+++ src/main/java/com/chat/chat/user/ChatUserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatUserRepository extends JpaRepository<ChatUser, Long> {
+    ChatUser findByUsername(String username);
 }
