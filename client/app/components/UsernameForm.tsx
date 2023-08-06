4b5aec4e457aca1211ca68f9f2860568c5b1600b
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-06 21:08:53 +0200

    Moved usernameError state to parent component

--- client/app/components/UsernameForm.tsx
+++ client/app/components/UsernameForm.tsx
@@ -1,24 +1,37 @@
-'use client';
-import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
-import UsernameSubmitButton from './UsernameSubmitButton';
-import { useState } from 'react';
+"use client";
+import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
+import UsernameSubmitButton from "./UsernameSubmitButton";
+import { useState } from "react";
 
 export default function UsernameForm() {
-
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState("");
+    const [usernameError, setUsernameError] = useState("");
 
     const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value)
-    }
+        setUsername(event.target.value);
+        setUsernameError("");
+    };
 
     return (
         <>
             <FormControl>
-                <InputLabel htmlFor="username" required>Username</InputLabel>
-                <Input onChange={handleUsername} id="username" aria-describedby="username" />
-                <FormHelperText id="username">Please create a username.</FormHelperText>
-                <UsernameSubmitButton username={username} />
+                <InputLabel htmlFor="username" required>
+                    Username
+                </InputLabel>
+                <Input
+                    onChange={handleUsername}
+                    id="username"
+                    aria-describedby="username"
+                />
+                <FormHelperText id="username">
+                    Please create a username.
+                </FormHelperText>
+                <UsernameSubmitButton
+                    username={username}
+                    setUsernameError={setUsernameError}
+                />
+                {usernameError && <div>{usernameError}</div>}
             </FormControl>
         </>
-    )
+    );
 }
--- client/app/components/UsernameSubmitButton.tsx
+++ client/app/components/UsernameSubmitButton.tsx
@@ -1,31 +1,43 @@
 "use client";
 import { Button } from "@mui/material";
 import axios from "axios";
+import router, { useRouter } from "next/router";
 
 type UsernameProps = {
-  username: string;
+    username: string;
+    setUsernameError: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export default function UsernameSubmitButton(username: UsernameProps) {
-  const handleUsernameSubmit = () => {
-    axios
-      .post(
-        "http://localhost:8080/api/users",
-        {
-          "username": username.username,
-        }
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+export default function UsernameSubmitButton({
+    username,
+    setUsernameError,
+}: UsernameProps) {
+    
+    const handleUsernameSubmit = () => {
+        axios
+            .post("http://localhost:8080/api/users", {
+                username: username,
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                if (
+                    error.response &&
+                    error.response.data &&
+                    error.response.data.error
+                ) {
+                    setUsernameError(error.response.data.error);
+                } else {
+                    setUsernameError("An unexpected error occurred.");
+                }
+                console.error(error);
+            });
+    };
 
-  return (
-    <>
-      <Button onClick={handleUsernameSubmit}>Create</Button>
-    </>
-  );
+    return (
+        <>
+            <Button onClick={handleUsernameSubmit}>Create</Button>
+        </>
+    );
 }
