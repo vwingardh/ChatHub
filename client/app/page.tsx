b30596776f27c4cca3a10457bb7d5ad3ca11f75c
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-06 21:13:17 +0200

    Removed Image import

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import ChatButton from './components/ChatButton'
+import ChatButton from "./components/ChatButton";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ChatButton></ChatButton>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <ChatButton></ChatButton>
+        </main>
+    );
 }
