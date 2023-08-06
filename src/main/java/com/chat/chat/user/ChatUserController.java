9b474aaf3c79c1f05acb189abd621f4878293953
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-06 21:09:44 +0200

    Created try/catch for username exceptions

--- src/main/java/com/chat/chat/user/ChatUserController.java
+++ src/main/java/com/chat/chat/user/ChatUserController.java
@@ -1,8 +1,13 @@
 package com.chat.chat.user;
 
+import com.chat.chat.customExceptions.InvalidUsernameLengthException;
+import com.chat.chat.customExceptions.UsernameAlreadyTakenException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,7 +32,20 @@ public class ChatUserController {
     }
 
     @PostMapping("users")
-    public ChatUser createUser(@RequestBody ChatUserDto userDto) {
-        return service.createUser(userDto.username());
+    public ResponseEntity<ApiResponse> createUser(@RequestBody ChatUserDto userDto) {
+        ApiResponse response = new ApiResponse();
+        try {
+            ChatUser newUser = service.createUser(userDto.username());
+            response.setData(newUser);
+            return ResponseEntity.created(URI.create("/api/users/" + newUser.getId().toString()))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response);
+        } catch (InvalidUsernameLengthException e) {
+            response.setError(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        } catch (UsernameAlreadyTakenException e) {
+            response.setError(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
