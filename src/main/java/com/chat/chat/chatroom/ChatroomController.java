56ef751f6a0363c5a1e289bed9564878c22436a9
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-07 11:19:12 +0200

    Updated createChatroom() to catch exceptions and send ResponseEntity

--- src/main/java/com/chat/chat/chatroom/ChatroomController.java
+++ src/main/java/com/chat/chat/chatroom/ChatroomController.java
@@ -1,9 +1,14 @@
 package com.chat.chat.chatroom;
 
 import com.chat.chat.customApiResponses.ApiResponse;
+import com.chat.chat.customExceptions.ChatroomNameAlreadyTakenException;
+import com.chat.chat.customExceptions.InvalidChatroomNameLengthException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/")
@@ -23,9 +28,23 @@ public class ChatroomController {
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping("chatroom")
-    public Chatroom createChatroom(@RequestBody String chatroomName) {
-        return service.createChatroom(chatroomName);
+    @PostMapping("chatrooms")
+    public ResponseEntity<ApiResponse> createChatroom(@RequestBody ChatroomDto chatroomDto) {
+        ApiResponse response = new ApiResponse();
+        try {
+            Chatroom chatroom = service.createChatroom(chatroomDto.chatroomName());
+            response.setData(chatroom);
+            return ResponseEntity.created(URI.create("/api/chatrooms/" + chatroom.getId().toString()))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(response);
+
+        } catch(ChatroomNameAlreadyTakenException e) {
+            response.setError(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        } catch (InvalidChatroomNameLengthException e) {
+            response.setError(e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
 }
