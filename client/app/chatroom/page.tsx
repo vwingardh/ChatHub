5ffe7761407f49a9bde949c5cc4f68efcd91c593
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-07 13:22:52 +0200

    Refactored HTML to include <main>

--- client/app/chatroom/page.tsx
+++ client/app/chatroom/page.tsx
@@ -2,8 +2,8 @@ import ChatroomNameForm from "../components/ChatroomNameForm";
 
 export default function Chat() {
     return (
-        <>
+        <main>
             <ChatroomNameForm />
-        </>
+        </main>
     );
 }
