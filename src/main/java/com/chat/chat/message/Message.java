a0fc1ee6c4b20b767e6b13a2c3593792936ee79b
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-08 13:53:58 +0200

    Message entity has relationship with User and Chatroom

--- src/main/java/com/chat/chat/message/Message.java
+++ src/main/java/com/chat/chat/message/Message.java
@@ -1,6 +1,10 @@
 package com.chat.chat.message;
 
+import com.chat.chat.chatroom.Chatroom;
+import com.chat.chat.user.ChatUser;
 import jakarta.persistence.*;
+import java.time.ZonedDateTime;
+
 
 @Entity
 @Table(name = "message")
@@ -13,13 +17,29 @@ public class Message {
     @Column(name = "message_text", nullable = false, length = 500)
     private String messageText;
 
+    private ZonedDateTime created;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ChatUser sender;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id")
+    private Chatroom chatroom;
+
     public Message() {
 
     }
 
-    public Message(Long id, String messageText) {
-        this.id = id;
+    public Message(String messageText, ChatUser sender, Chatroom chatroom) {
         this.messageText = messageText;
+        this.sender = sender;
+        this.chatroom = chatroom;
+    }
+
+    @PrePersist
+    public void setDateTimeCreated() {
+        this.created = ZonedDateTime.now();
     }
 
     public Long getId() {
@@ -34,4 +54,23 @@ public class Message {
         this.messageText = messageText;
     }
 
+    public ZonedDateTime getCreated() {
+        return created;
+    }
+
+    public ChatUser getSender() {
+        return sender;
+    }
+
+    public void setSender(ChatUser sender) {
+        this.sender = sender;
+    }
+
+    public Chatroom getChatroom() {
+        return chatroom;
+    }
+
+    public void setChatroom(Chatroom chatroom) {
+        this.chatroom = chatroom;
+    }
 }
