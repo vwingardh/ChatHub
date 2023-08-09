a3f14222ecf3dd94dbb24790a47b6884314bad39
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-09 10:13:56 +0200

    Updated ChatButton to skip username form if userId exists

--- client/app/components/ChatButton.tsx
+++ client/app/components/ChatButton.tsx
@@ -1,12 +1,23 @@
+"use client";
 import { Button } from "@mui/material";
 import Link from "next/link";
 
 export default function ChatButton() {
+    
+    const data = sessionStorage;
+    const userId = data.getItem("userId");
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Link href={"/user"}>
-                <Button>Chat Now</Button>
-            </Link>
+            {!userId ? (
+                <Link href="/user">
+                    <Button>Chat Now</Button>
+                </Link>
+            ) : (
+                <Link href="/chat-options">
+                    <Button>Chat Now</Button>
+                </Link>
+            )}
         </main>
     );
 }
