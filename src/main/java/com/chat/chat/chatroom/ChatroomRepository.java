ede99605ebc1fe0843759375d195a186c150fc8d
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-09 17:37:50 +0200

    Created findByLink() to retrieve chatroom by join link

--- src/main/java/com/chat/chat/chatroom/ChatroomRepository.java
+++ src/main/java/com/chat/chat/chatroom/ChatroomRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
 
     Chatroom findByChatroomName(String chatroomName);
+    Chatroom findByLink(String joinLink);
 }
