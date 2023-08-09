2961e635b04e8610acf5383678464462ed043724
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-09 11:50:16 +0200

    Refactored messaging to chatroom-lobby page

--- client/app/chatroom-lobby/page.tsx
+++ client/app/chatroom-lobby/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DisplayMessage from "../components/DisplayMessage";
+import axios from "axios";
+import { useRouter } from "next/router";
 import MessageForm from "../components/MessageForm";
 
-export default function ChatroomMessaging() {
+
+export default function ChatroomLobby() {
 
     const [messageTextError, setMessageTextError] = useState("");
     const [messageId, setMessageId] = useState("");
