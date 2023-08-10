819bd1e4d2de292bcd94d0efac58eb324612950c
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-10 18:57:32 +0200

    Updated dependencies and configuration files for websocket

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          net: false,
+        };
+      
+        return config;
+      },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
