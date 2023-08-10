86f9f88aeef26c6622c0958c5e137b4742ef910d
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-10 18:49:59 +0200

    ChannelLobby now handles websocket and retrieves list of messages

--- client/app/channel-lobby/page.tsx
+++ client/app/channel-lobby/page.tsx
@@ -2,20 +2,71 @@
 import { useEffect, useState } from "react";
 import DisplayMessage from "../components/DisplayMessage";
 import axios from "axios";
-import { useRouter } from "next/router";
 import MessageForm from "../components/MessageForm";
+import { connect, disconnect } from "../lib/websocket"
 
+
+type MessageDtoResponse = {
+    id: string;
+    messageText: string;
+    created: string;
+}
+
+type StompMessageFrame = {
+    body: string;
+};
 
 export default function ChannelLobby() {
 
-    const [messageTextError, setMessageTextError] = useState("");
-    const [messageId, setMessageId] = useState("");
+    const [messages, setMessages] = useState<Array<MessageDtoResponse>>([]);
+    const [messageError, setMessageError] = useState("")
+
+    useEffect(() => {
+        const channelId = sessionStorage.getItem("channelId");
+
+        axios
+        .get("http://localhost:8080/api/messages/channel/" + channelId)
+        .then((response) => {
+            setMessages(response.data.data);
+        })
+        .catch((error) => {
+            if (
+                error.response &&
+                error.response.data &&
+                error.response.data.error
+            ) {
+                setMessageError(error.response.data.error);
+            } else {
+                setMessageError("An unexpected error occurred.");
+            }
+        });
+
+        if (channelId) {
+            connect(channelId, (messageFrame: StompMessageFrame) => {
+                try {
+                    const newMessage = JSON.parse(messageFrame.body);
+                    console.log(newMessage)
+                    setMessages(prevMessages => [...prevMessages, newMessage]);
+                } catch (error) {
+                    console.error("Error parsing WebSocket payload: ", error);
+                }
+            })
+        }
+
+        return () => {
+            disconnect();
+        };
+    }, []);
+
+    const handleSendMessage = (newMessage: MessageDtoResponse) => {
+        setMessages(prevMessages => [...prevMessages, newMessage]);
+    }
 
     return (
         <main>
-            <DisplayMessage messageId={messageId} joinLink={null} />
-            <MessageForm setMessageTextError={setMessageTextError} setMessageId={setMessageId}/>
-            {messageTextError && <div>{messageTextError}</div>}
+            <DisplayMessage messages={messages} />
+            <MessageForm onSend={handleSendMessage} />
+            {messageError && <div>{messageError}</div>}
         </main>
     )
 }
