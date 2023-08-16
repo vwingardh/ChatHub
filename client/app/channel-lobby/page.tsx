f90e712bde7bb38735aa8cece4e3a9de7e894075
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-16 16:54:38 +0200

    Created functionality to increment and decrement active user count

--- client/app/channel-lobby/page.tsx
+++ client/app/channel-lobby/page.tsx
@@ -3,70 +3,130 @@ import { useEffect, useState } from "react";
 import DisplayMessage from "../components/DisplayMessage";
 import axios from "axios";
 import MessageForm from "../components/MessageForm";
-import { connect, disconnect } from "../lib/websocket"
+import { connect, disconnect } from "../lib/websocket";
+import { DisplayActiveUsers } from "./DisplayActiveUsers";
 
 
 type MessageDtoResponse = {
     id: string;
     messageText: string;
     created: string;
-}
+};
 
 type StompMessageFrame = {
     body: string;
 };
 
-export default function ChannelLobby() {
+type ChannelProps = {
+    channelId: string | null;
+};
 
+export default function ChannelLobby() {
     const [messages, setMessages] = useState<Array<MessageDtoResponse>>([]);
-    const [messageError, setMessageError] = useState("")
+    const [messageError, setMessageError] = useState("");
+    const [activeUsers, setActiveUsers] = useState(0);
+
+    const channelId = sessionStorage.getItem("channelId");
+
+    const joinChannel = async (channelId: ChannelProps) => {
+        try {
+            await axios.post(
+                `http://localhost:8080/api/channels/${channelId}/join-channel`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+        } catch (error) {
+            console.error("Error joining channel: ", error);
+        }
+    };
+
+    const leaveChannel = async (channelId: ChannelProps) => {
+        try {
+            await axios.patch(
+                `http://localhost:8080/api/channels/${channelId}/decrement-users`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleActiveUsersUpdate = (message) => {
+        setActiveUsers(JSON.parse(message.body));
+    };
 
     useEffect(() => {
-        const channelId = sessionStorage.getItem("channelId");
+        axios
+            .get("http://localhost:8080/api/messages/channel/" + channelId)
+            .then((response) => {
+                setMessages(response.data.data);
+            })
+            .catch((error) => {
+                if (
+                    error.response &&
+                    error.response.data &&
+                    error.response.data.error
+                ) {
+                    setMessageError(error.response.data.error);
+                } else {
+                    setMessageError("An unexpected error occurred.");
+                }
+            });
 
         axios
-        .get("http://localhost:8080/api/messages/channel/" + channelId)
-        .then((response) => {
-            setMessages(response.data.data);
-        })
-        .catch((error) => {
-            if (
-                error.response &&
-                error.response.data &&
-                error.response.data.error
-            ) {
-                setMessageError(error.response.data.error);
-            } else {
-                setMessageError("An unexpected error occurred.");
-            }
-        });
+            .get(`http://localhost:8080/api/channels/${channelId}/active-users`)
+            .then((response) => {
+                setActiveUsers(response.data.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
 
         if (channelId) {
             connect(channelId, (messageFrame: StompMessageFrame) => {
-                try {
-                    const newMessage = JSON.parse(messageFrame.body);
-                    console.log(newMessage)
-                    setMessages(prevMessages => [...prevMessages, newMessage]);
-                } catch (error) {
-                    console.error("Error parsing WebSocket payload: ", error);
-                }
-            })
+                    try {
+                        const newMessage = JSON.parse(messageFrame.body);
+                        setMessages((prevMessages) => [
+                            ...prevMessages,
+                            newMessage,
+                        ]);
+                    } catch (error) {
+                        console.error(
+                            "Error parsing WebSocket payload: ",
+                            error
+                        );
+                    }
+                },
+                handleActiveUsersUpdate
+            );
+            joinChannel(channelId);
         }
 
         return () => {
             disconnect();
+            if (channelId) {
+                leaveChannel(channelId);
+            }
         };
-    }, []);
+    }, [channelId]);
 
     const handleSendMessage = (newMessage: MessageDtoResponse) => {
-        setMessages(prevMessages => [...prevMessages, newMessage]);
-    }
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+    };
 
     return (
         <main>
             <DisplayMessage messages={messages} />
+            <DisplayActiveUsers activeUsers={activeUsers} />
             <MessageForm onSend={handleSendMessage} />
             {messageError && <div>{messageError}</div>}
         </main>
-    )
+    );
 }
