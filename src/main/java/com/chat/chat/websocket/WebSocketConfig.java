c9f3f9a418ad155a51cb42a38af1b11637a50cae
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-16 17:17:09 +0200

    Updated broker with active-users

--- src/main/java/com/chat/chat/websocket/WebSocketConfig.java
+++ src/main/java/com/chat/chat/websocket/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/channel");
+        config.enableSimpleBroker("/channel", "active-users");
         config.setApplicationDestinationPrefixes("/app");
     }
 
