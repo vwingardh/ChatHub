8c11d1e882ec024cd77e2ddb041da6ac24aca8df
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-18 20:34:53 +0200

    Refactored code and added styling

--- client/app/components/MessageForm.tsx
+++ client/app/components/MessageForm.tsx
@@ -1,55 +1,62 @@
 "use client";
-import { FormControl, InputLabel, Input, Button } from "@mui/material";
+import {
+    FormControl,
+    InputLabel,
+    Input,
+    Button,
+    Container,
+} from "@mui/material";
 import { useState } from "react";
 import axios from "axios";
 import { sendMessage } from "../lib/websocket";
 
-
-type MessageFormProps = {
-    onSend: (message: any) => void;
-};
-
-
-export default function MessageForm({ onSend }: MessageFormProps) {
+export default function MessageForm() {
     const [messageText, setMessageText] = useState("");
     const userId = sessionStorage.getItem("userId");
     const channelId = sessionStorage.getItem("channelId");
+    const channelName = sessionStorage.getItem("channelName");
 
     const handleMessageText = (event: React.ChangeEvent<HTMLInputElement>) => {
         setMessageText(event.target.value);
     };
 
     const handleMessageSubmit = () => {
-        axios.post("http://localhost:8080/api/messages", {
-            messageText: messageText,
-            userId: userId,
-            channelId: channelId
-        })
-        .then((response) => {
-            sendMessage(messageText, userId, channelId)
-            setMessageText("");
-        })
-        .catch((error) => {
-            console.error(error);
-        })
+        if (channelId) {
+            axios
+                .post("http://localhost:8080/api/messages", {
+                    messageText: messageText,
+                    userId: userId,
+                    channelId: channelId,
+                })
+                .then((response) => {
+                    sendMessage(messageText, userId, channelId);
+                    setMessageText("");
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
     };
 
     return (
-        <>
-            <FormControl style={{ marginTop: "200px", marginLeft: "200px" }}>
-                <InputLabel htmlFor="username" required>
-                    Type message
-                </InputLabel>
+        <Container className="container--message-form">
+            <FormControl className="form">
+                <InputLabel htmlFor="message">Message {channelName}</InputLabel>
                 <Input
                     value={messageText}
                     onChange={handleMessageText}
-                    id="username"
-                    aria-describedby="username"
+                    id="message"
+                    aria-describedby="message"
                     inputProps={{ maxLength: 500 }}
                     required
                 />
-                <Button onClick={handleMessageSubmit}>Post</Button>
+                <Button
+                    className="button--submit"
+                    onClick={handleMessageSubmit}
+                >
+                    Post
+                </Button>
             </FormControl>
-        </>
+        </Container>
     );
 }
