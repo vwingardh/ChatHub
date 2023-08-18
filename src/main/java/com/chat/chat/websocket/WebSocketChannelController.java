a4d9c1bfe26381db9d431483fa06d4e4bcb9424a
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-18 20:40:41 +0200

    Removed MessageDtoResponse to fix sender undefined error

--- src/main/java/com/chat/chat/websocket/WebSocketChannelController.java
+++ src/main/java/com/chat/chat/websocket/WebSocketChannelController.java
@@ -4,7 +4,6 @@ import com.chat.chat.channel.ChannelService;
 import com.chat.chat.customExceptions.MismatchedChannelException;
 import com.chat.chat.message.Message;
 import com.chat.chat.message.MessageDtoRequest;
-import com.chat.chat.message.MessageDtoResponse;
 import com.chat.chat.message.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -27,13 +26,12 @@ public class WebSocketChannelController {
 
     @MessageMapping("/channel/{channelId}")
     @SendTo("/channel/{channelId}")
-    public MessageDtoResponse broadcastMessage(@DestinationVariable Long channelId, MessageDtoRequest messageDto) {
+    public Message broadcastMessage(@DestinationVariable Long channelId, MessageDtoRequest messageDto) {
         if (!channelId.equals(messageDto.channelId())) {
             throw new MismatchedChannelException("Provided channel ID does not match the message's channel ID");
         }
         Message message = messageService.createMessage(messageDto.messageText(), messageDto.userId(), messageDto.channelId());
-        MessageDtoResponse messageResponse = new MessageDtoResponse(message.getId(), message.getMessageText(), message.getCreated());
-        return messageResponse;
+        return message;
     }
 
     @MessageMapping("/active-users/{channelId}")
