5503c5e1eb7e096fa95303c59c87d11436fbdfa4
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-18 20:29:25 +0200

    Refactored ChatOptions to simplified solution - removed ternary

--- client/app/chat-options/page.tsx
+++ client/app/chat-options/page.tsx
@@ -1,12 +1,30 @@
+import { Button, Container } from "@mui/material";
 import Link from "next/link";
 
-
 export default function ChatOptions() {
+    const chatOptions = [
+        { label: "Create Channel", href: "/create-channel" },
+        {
+            label: "Join Channel",
+            href: "/join-channel",
+        },
+    ];
 
     return (
         <main>
-            <Link href={"/create-channel"} style={{ color: 'red' }}>create channel</Link>
-            <Link href={"/join-channel"} style={{ color: 'blue' }}>join channel</Link>
+            <Container className="container--standard">
+                {chatOptions.map((option) => (
+                    <Link key={option.label} href={option.href}>
+                        <Button
+                            style={{ marginBottom: "50px" }}
+                            className="button--route"
+                            variant="contained"
+                        >
+                            {option.label}
+                        </Button>
+                    </Link>
+                ))}
+            </Container>
         </main>
-    )
+    );
 }
