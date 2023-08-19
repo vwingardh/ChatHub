81730742278b3777ffe2fda81a866d4e240727a0
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-19 10:26:55 +0200

    Refactored: created helper methods to find channels or throw

--- src/main/java/com/chat/chat/channel/ChannelService.java
+++ src/main/java/com/chat/chat/channel/ChannelService.java
@@ -8,7 +8,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Set;
 
 
@@ -25,35 +24,20 @@ public class ChannelService {
         this.redisTemplate = redisTemplate;
     }
 
-    public void printAllKeys() {
-        Set<String> keys = redisTemplate.keys("*");
-        for (String key : keys) {
-            String value = redisTemplate.opsForValue().get(key);
-            System.out.println(key + ": " + value);
-        }
-    }
-
-
-
     public Channel createChannel(String channelName) throws ChannelNameAlreadyTakenException {
         if (channelName.length() <= 4 || channelName.length() >= 31) {
             throw new InvalidChannelNameLengthException(channelName.length());
         }
-        Channel isChannelAvailable = repo.findByChannelName(channelName);
-        if (isChannelAvailable != null) {
-            throw new ChannelNameAlreadyTakenException(channelName);
-        }
+        isChannelAvailableElseThrow(channelName);
+
         Channel channel = new Channel();
         channel.setName(channelName.trim());
         return repo.save(channel);
     }
 
-    public void deleteChannelById(Long id) {
-        Optional<Channel> channel = repo.findById(id);
-        if (!channel.isPresent()) {
-            throw new NoSuchElementException(String.format("Channel with id '%s' does not exist", id));
-        }
-        repo.deleteById(id);
+    public void deleteChannelById(Long channelId) {
+        Channel channel = findChannelByIdOrElseThrow(channelId);
+        repo.deleteById(channel.getId());
     }
 
     public List<Channel> getAllChannels() {
@@ -61,27 +45,15 @@ public class ChannelService {
     }
 
     public Channel getChannelByChannelName(String channelName) throws NoSuchElementException {
-        Channel channel = repo.findByChannelName(channelName);
-        if (channel == null) {
-            throw new NoSuchElementException(String.format("Channel with name '%s' does not exist", channelName));
-        }
-        return channel;
+        return findChannelByChannelNameOrElseThrow(channelName);
     }
 
-    public Channel getChannelById(Long id) throws NoSuchElementException {
-        Optional<Channel> channel = repo.findById(id);
-        if (!channel.isPresent()) {
-            throw new NoSuchElementException(String.format("Channel with id '%s' does not exist", id));
-        }
-        return channel.get();
+    public void getChannelById(Long channelId) throws NoSuchElementException {
+        findChannelByIdOrElseThrow(channelId);
     }
 
     public Channel getChannelByChannelLink(String joinLink) {
-        Channel channel = repo.findByLink(joinLink);
-        if (channel == null) {
-            throw new NoSuchElementException(String.format("Channel with link '%s' does not exist", joinLink));
-        }
-        return channel;
+        return findChannelByLinkOrElseThrow(joinLink);
     }
 
     public void incrementActiveUsers(Long channelId) {
@@ -97,6 +69,42 @@ public class ChannelService {
     public Long getActiveUsers(Long channelId) {
         String key = "channel:" + channelId + ":activeUsers";
         return redisTemplate.opsForValue().increment(key, 0);
+    }
+
+    public void printAllKeys() {
+        Set<String> keys = redisTemplate.keys("*");
+        for (String key : keys) {
+            String value = redisTemplate.opsForValue().get(key);
+            System.out.println(key + ": " + value);
+        }
+    }
+
+    private void isChannelAvailableElseThrow(String channelName) {
+        Channel isChannelAvailable = repo.findByChannelName(channelName);
+        if (isChannelAvailable != null) {
+            throw new ChannelNameAlreadyTakenException(channelName);
+        }
+    }
+
+    private Channel findChannelByIdOrElseThrow(Long channelId) {
+            return repo.findById(channelId).orElseThrow(() ->
+                    new NoSuchElementException(String.format("Channel with id '%s' does not exist", channelId)));
+    }
+
+    private Channel findChannelByChannelNameOrElseThrow(String channelName) {
+        Channel channel = repo.findByChannelName(channelName);
+        if (channel == null) {
+            throw new NoSuchElementException(String.format("Channel with name '%s' does not exist", channelName));
+        }
+        return channel;
+    }
+
+    private Channel findChannelByLinkOrElseThrow(String joinLink) {
+        Channel channel = repo.findByLink(joinLink);
+        if (channel == null) {
+            throw new NoSuchElementException(String.format("Channel with link '%s' does not exist", joinLink));
+        }
+        return channel;
     }
 
 }
