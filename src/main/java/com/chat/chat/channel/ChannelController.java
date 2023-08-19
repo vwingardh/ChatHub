07afc85e1054138a40def315ce4ffeed0ff5dea9
Vanessa Wingårdh <vwingardh@gmail.com>
2023-08-19 10:25:58 +0200

    Refactored: ApiResponse is autowired

--- src/main/java/com/chat/chat/channel/ChannelController.java
+++ src/main/java/com/chat/chat/channel/ChannelController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 
@@ -16,22 +15,22 @@ import java.util.List;
 public class ChannelController {
 
     private final ChannelService service;
+    private final ApiResponse response;
 
     @Autowired
-    public ChannelController(ChannelService service) {
+    public ChannelController(ChannelService service, ApiResponse response) {
         this.service = service;
+        this.response = response;
     }
 
     @GetMapping("channel-status")
     public ResponseEntity<ApiResponse> getStatusCode200() {
-        ApiResponse response = new ApiResponse();
         response.setStatus("API is operational");
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("channels")
     public ResponseEntity<ApiResponse> getAllChannels() {
-        ApiResponse response = new ApiResponse();
         List<Channel> channelList = service.getAllChannels();
         response.setData(channelList);
         return ResponseEntity.ok().body(response);
@@ -39,7 +38,6 @@ public class ChannelController {
 
     @GetMapping("channels/{joinLink}")
     public ResponseEntity<ApiResponse> getChannelByChannelLink(@PathVariable String joinLink) {
-        ApiResponse response = new ApiResponse();
         Channel channel = service.getChannelByChannelLink(joinLink);
         response.setData(channel);
         return ResponseEntity.ok().body(response);
@@ -47,7 +45,6 @@ public class ChannelController {
 
     @GetMapping("channels/{channelId}/active-users")
     public ResponseEntity<ApiResponse> getActiveUserCount(@PathVariable String channelId) {
-        ApiResponse response = new ApiResponse();
         Long activeUsers = service.getActiveUsers(Long.valueOf(channelId));
         response.setData(activeUsers);
         return ResponseEntity.ok().body(response);
@@ -55,7 +52,6 @@ public class ChannelController {
 
     @PostMapping("channels/{channelId}/join-channel")
     public ResponseEntity<ApiResponse> joinChannel(@PathVariable String channelId) {
-        ApiResponse response = new ApiResponse();
         response.setStatus("User added to channel");
         service.incrementActiveUsers(Long.valueOf(channelId));
         return ResponseEntity.ok().body(response);
@@ -63,7 +59,6 @@ public class ChannelController {
 
     @PatchMapping("channels/{channelId}/decrement-users")
     public ResponseEntity<ApiResponse> decrementActiveUsers(@PathVariable String channelId) {
-        ApiResponse response = new ApiResponse();
         response.setStatus("User removed from channel");
         service.decrementActiveUsers(Long.valueOf(channelId));
         return ResponseEntity.ok().body(response);
@@ -71,7 +66,6 @@ public class ChannelController {
 
     @PostMapping("channels")
     public ResponseEntity<ApiResponse> createChannel(@RequestBody ChannelDto channelDto) {
-        ApiResponse response = new ApiResponse();
         try {
             Channel channel = service.createChannel(channelDto.channelName());
             response.setData(channel);
